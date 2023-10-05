d8b6fdf5f33737fa075f0ecdf976b6fdfb26b698
Nyagol <nyagolpeter@gmail.com>
2023-10-05 16:13:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  let result;
+  if (distance <= 400) {
+    result = "This one is on me!";
+  } else if (distance >= 2000 && distance <= 2500) {
+    result = "I will gladly take your thirty bucks.";
+  } else if (distance > 2500) {
+    result = "No can do.";
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generous) {
+  switch (generous) {
+    case "generous":
+      return "Thank you so much."
+      // console.log("Thank you so much.");
+      break;
+    case "not as generous":
+      return "Thank you."
+      // console.log("Thank you.");
+      break;
+
+    default:
+      return "Bye."
+      // console.log("Bye.");
+  }
 }
+
+
